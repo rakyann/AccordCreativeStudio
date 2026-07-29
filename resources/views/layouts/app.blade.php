9a9325995991aab568d0d98a11ac83eb29e0e168
Rakyan <rkyan22@gmail.com>
2026-07-29 17:28:08 +0700

    fix: route static assets directly to Vercel CDN and update app.blade.php to root-relative paths

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bruno+Ace&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,600&family=Italiana&family=Krona+One&family=Michroma&family=Outfit:wght@700;800&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Syne:wght@700;800&family=Zen+Dots&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="/css/style.css?v=1.0.1">
     
     @stack('styles')
 </head>
@@ -20,7 +20,7 @@
     @yield('content')
 
     <!-- Custom JS -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/js/app.js?v=1.0.1"></script>
     
     @stack('scripts')
 </body>
